eab63e49bcf570cbf62c8ac08d76f90ea09fbb9d
Liana Liu <liana9328@gmail.com>
2021-01-07 23:02:59 +1100

    Updates

---
+++ precache-manifest.8a905fa0100c350bb8a36b92ac4c7aaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a1e938bc6501ed5db55fe03064047f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f301924ee39ea841c13b",
+    "url": "/monsters-rolodex/static/css/main.f8439aa0.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "f301924ee39ea841c13b",
+    "url": "/monsters-rolodex/static/js/main.9b3d26a4.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
